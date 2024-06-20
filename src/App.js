0e4d2ddad23b2e0a8a6dee8257887c6ffe3cf48b
NDK <ndk1200@gmail.com>
2024-06-21 01:21:36 +0700

    final app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const api = {
   key: "cb34af209ce1dabfd70976a0af39ce53",
@@ -8,7 +9,7 @@ const api = {
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [searchCity, setSearchCity] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState({});
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -27,9 +28,8 @@ function App() {
         const response = await fetch(url);
         const data = await response.json();
         if (response.ok) {
-          setWeatherInfo(
-            `${data.name}, ${data.sys.country} ${data.weather[0].description}, ${data.main.temp}`
-          );
+          setWeatherInfo(data);
+          console.log(weatherInfo);
           setErrorMessage("");
         } else {
           setErrorMessage(data.message);
@@ -44,15 +44,18 @@ function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="City"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <button>Search</button>
-      </form>
+      <div className="input-group">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search for any city"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <span className="highlight"></span>
+          <span className="bar"></span>
+        </form>
+      </div>
       {loading ? (
         <div>Loading...</div>
       ) : (
@@ -60,7 +63,62 @@ function App() {
           {errorMessage ? (
             <div style={{ color: "red" }}>{errorMessage}</div>
           ) : (
-            <div>{weatherInfo}</div>
+            weatherInfo.main && (
+              <div className="container">
+                <div className="weather-side">
+                  <div className="weather-gradient"></div>
+                  <div className="date-container">
+                    <h2 className="date-dayname">
+                      {new Date().toLocaleDateString("en-US", {
+                        weekday: "long",
+                      })}
+                    </h2>
+                    <span className="date-day">
+                      {new Date().toLocaleDateString()}
+                    </span>
+                    <span className="location">{weatherInfo.name}</span>
+                  </div>
+                  <div className="weather-container">
+                    <h1 className="weather-temp">{weatherInfo.main.temp}°C</h1>
+                    <h3 className="weather-desc">{weatherInfo.description}</h3>
+                  </div>
+                </div>
+                <div className="info-side">
+                  <div className="today-info-container">
+                    <div className="today-info">
+                      <div className="precipitation">
+                        <span className="title">Feels Like - </span>
+                        <span className="value">
+                          {weatherInfo.main.feels_like}°C
+                        </span>
+                        <div className="clear"></div>
+                      </div>
+                      <div className="humidity">
+                        <span className="title">HUMIDITY - </span>
+                        <span className="value">
+                          {weatherInfo.main.humidity}%
+                        </span>
+                        <div className="clear"></div>
+                      </div>
+                      <div className="wind">
+                        <span className="title">WIND - </span>
+                        <span className="value">
+                          {weatherInfo.wind.speed} km/h
+                        </span>
+                        <div className="clear"></div>
+                      </div>
+                      <div className="pressure">
+                        <span className="title">PRESSURE - </span>
+                        <span className="value">
+                          {weatherInfo.main.pressure} mbar
+                        </span>
+                        <div className="clear"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
           )}
         </>
       )}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import React from "react";
 import App from "./App";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
